6faea236c677b71c6959abcf69c28982f4c93300
ZIHAD22 <mdzihad2244@gmail.com>
2021-12-24 21:49:51 +0600

    the project is stop for lag of knowlage

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = express();
+dotenv.config();
+
+// databas connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection sussesfull"))
+  .catch((err) => console.log(err));
